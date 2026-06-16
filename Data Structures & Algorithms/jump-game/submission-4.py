e8a442eb90234ce4f6ce42b086f569b80ab13b91
SSneham <105969383+SSneham@users.noreply.github.com>
2026-06-16 12:10:28 +0530

    Add: jump-game - submission-4

---
+++ Data Structures & Algorithms/jump-game/submission-4.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        maxind = 0
+        for i in range(n):
+            if i > maxind:
+                return False
+            maxind = max(maxind, i+nums[i])
+        return True
